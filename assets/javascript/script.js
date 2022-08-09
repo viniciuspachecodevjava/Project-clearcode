09507c6fcfb51fcc7c74829268fbf2101560f109
viniciuspch <vinicius.geral2018@gmail.com>
2022-08-09 10:29:02 -0300

    Arquivo javascript adicionado via repositório local

---
+++ assets/javascript/script.js
@@ -0,0 +1,38 @@
+function initAnimateSmoth() {
+  const animationSection = document.querySelectorAll('.js-scroll');
+  const metadeWindow = window.innerHeight * 0.6;
+
+  function sectionTop() {
+    animationSection.forEach((element) => {
+      const elementTop = element.getBoundingClientRect().top - metadeWindow;
+      const classActive = 'ativo';
+
+      console.log(elementTop);
+      if (elementTop < 0) {
+        element.classList.add(classActive);
+      }
+    })
+  }
+  window.addEventListener('scroll', sectionTop);
+}
+function initAnimateScroll() {
+  const linkInterno = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  linkInterno.forEach((item) => {
+    item.addEventListener('click', animateScroll);
+  })
+
+  function animateScroll(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const selectHref = document.querySelector(href);
+
+    selectHref.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  }
+}
+
+initAnimateSmoth();
+initAnimateScroll();
